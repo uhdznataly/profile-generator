1d9353ce2cbdb08fe07fae23af9ac8fe39c4fc9a
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2023-01-22 18:25:27 -0600

    changed test teamMember name and email

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -4,15 +4,15 @@ const Employee = require("../lib/employee");
 
 describe("Employee class constructor", () => {
     it("New employee creation", () => {
-        const teamMember = new Employee("Gimli", "1", "gimili@middleearth.com");
-        expect(teamMember.name).toEqual("Gimli");
+        const teamMember = new Employee("Legolas", "1", "legolas@middleearth.com");
+        expect(teamMember.name).toEqual("Legolas");
         expect(teamMember.id).toEqual("1");
-        expect(teamMember.email).toEqual("gimli@middleearth.com");
+        expect(teamMember.email).toEqual("legolas@middleearth.com");
     }),
 
         it("getName Function", () => {
-            const teamMember = new Employee("Gimli", "1", "gimili@middleearth.com");
-            expect(teamMember.getName()).toEqual("Gimli");
+            const teamMember = new Employee("Legolas", "1", "legolas@middleearth.com");
+            expect(teamMember.getName()).toEqual("Legolas");
 
 
         });
