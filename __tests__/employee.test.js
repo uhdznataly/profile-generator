599de1de27bed5538a495d2463441388fbe967e4
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2023-01-22 18:21:18 -0600

    add Employee javascript test

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,2 +1,20 @@
 //load Employee javascript file
 
+const Employee = require("../lib/employee");
+
+describe("Employee class constructor", () => {
+    it("New employee creation", () => {
+        const teamMember = new Employee("Gimli", "1", "gimili@middleearth.com");
+        expect(teamMember.name).toEqual("Gimli");
+        expect(teamMember.id).toEqual("1");
+        expect(teamMember.email).toEqual("gimli@middleearth.com");
+    }),
+
+        it("getName Function", () => {
+            const teamMember = new Employee("Gimli", "1", "gimili@middleearth.com");
+            expect(teamMember.getName()).toEqual("Gimli");
+
+
+        });
+
+});
